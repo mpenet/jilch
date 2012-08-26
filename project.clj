a05bde7153767256b0a505de9f10503b0d20b7ee
Max Penet <max.penet@gmail.com>
2012-08-26 20:34:59 +0200

    update to lein2 project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
-(defproject jilch "0.1.0-SNAPSHOT"
+(defproject cc.qbits/jilch "0.1.0-SNAPSHOT"
   :description "Jilch: A Clojure 0MQ Messaging Library (using jeromq)"
-  :repositories {"sonatype" "https://oss.sonatype.org/content/repositories/releases"}
+  :url "https://github.com/mpenet/jilch"
+  :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
+                 "sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.jeromq/jeromq "0.1.0-SNAPSHOT"]])
+                 [org.jeromq/jeromq "0.1.0-SNAPSHOT"]]
+  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+             :test {:dependencies []}}
+  :min-lein-version "2.0.0")
