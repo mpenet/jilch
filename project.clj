20d038852839fd21abf9fdde5e8eac11ff67f2c3
Max Penet <max.penet@gmail.com>
2012-08-26 20:35:40 +0200

    remove min-lein-version

--- project.clj
+++ project.clj
@@ -7,5 +7,4 @@
                  [org.jeromq/jeromq "0.1.0-SNAPSHOT"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-             :test {:dependencies []}}
-  :min-lein-version "2.0.0")
+             :test {:dependencies []}})
