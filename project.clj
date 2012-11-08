505333c64cf4c41ad72d2419100f46fabe52839f
Max Penet <m@qbits.cc>
2012-11-08 19:00:59 +0100

    bump jeromq dependency to 0.1.0 stable

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cc.qbits/jilch "0.1.0-SNAPSHOT"
+(defproject cc.qbits/jilch "0.1.0"
   :description "Jilch: A Clojure 0MQ Messaging Library (using jeromq)"
   :url "https://github.com/mpenet/jilch"
   :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
                  "sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.jeromq/jeromq "0.1.0-SNAPSHOT"]]
+                 [org.jeromq/jeromq "0.1.0"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
              :test {:dependencies []}})
